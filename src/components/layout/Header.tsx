c1f1ba661c53f21ae34e54fca923a9579cbfeb4a
ruffol <ruffol@webbacuario.local>
2026-07-01 13:39:08 -0600

    Fix: agregar Footer.navegacion a traducciones + eliminar link favoritos (404)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -87,14 +87,6 @@ export default function Header() {
 
             <ThemeToggle />
 
-
-            {/* Favorites */}
-            <Link href="/favoritos" className="hidden sm:flex p-2 text-blue-200/60 hover:text-white transition-colors rounded-lg hover:bg-white/5" aria-label="Favoritos">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-              </svg>
-            </Link>
-
             {/* Cart */}
             <button
               onClick={openCart}
